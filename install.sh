b3b15ab6710a4bbcb36cd37f0d3a9260b7a5fbfb
geilige <582102796@qq.com>
2022-11-14 19:12:08 +0800

    Update install.sh
    update pycharm-2022.2.3

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 cd ~
 sudo apt-get update && sudo apt-get install openjdk-8-jdk
-wget https://download.jetbrains.com/python/pycharm-professional-2019.3.4.tar.gz?_ga=2.42966822.2056165753.1586158936-1955479096.1586158936 -O pycharm-professional-2019.3.4.tar.gz
-tar -xzf pycharm-professional-2019.3.4.tar.gz && cd pycharm-2019.3.4/bin
+wget https://download-cdn.jetbrains.com/python/pycharm-professional-2022.2.3.tar.gz -O pycharm-professional-2022.2.3.tar.gz
+tar -xzf pycharm-professional-2022.2.3.tar.gz && cd pycharm-2022.2.3/bin
 sudo chmod +x pycharm.sh && mv pycharm.sh pycharm
-sudo rm -rf pycharm-professional-2019.3.4.tar.gz
+sudo rm -rf  pycharm-professional-2022.2.3.tar.gz
 cd ~
-echo 'export PATH=/home/'$USER'/pycharm-2019.3.4/bin:$PATH' >> .bashrc
+echo 'export PATH=/home/'$USER'/pycharm-2022.2.3/bin:$PATH' >> .bashrc
 echo " *** Congratualations! You have successfully installed pycharm-aarch64 !!"
 echo " *** Type 'pycharm' to start using pycharm."
 echo " *** Enjoy !!!"
